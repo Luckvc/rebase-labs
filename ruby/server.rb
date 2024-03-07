4fed14eb8a2aebaaccbe48be94e44481f6e85e69
Lucas Vasques <lucasvc1996@gmail.com>
2024-03-07 09:26:45 -0300

    Fix: tests were quering development db

--- ruby/server.rb
+++ ruby/server.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 require 'pg'
 require 'byebug'
 
-conn = PG.connect( dbname: 'development', host: 'rblabs-postgres', user: 'admin', password: '123456' )
+dbname = ENV['RACK_ENV']
+conn = PG.connect( dbname: dbname, host: 'rblabs-postgres', user: 'admin', password: '123456' )
 
 get '/tests' do
+  puts dbname
   content_type 'application/json'
   result = conn.exec("SELECT * FROM tests;").to_a
   result.to_json
